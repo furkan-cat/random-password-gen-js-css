382c1a65419a040e09817d7391ab8ed25d95ee66
Furkan Çat <75113766+furkan-cat@users.noreply.github.com>
2023-03-06 13:07:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,3 @@ function getPassword() {
     }
     document.getElementById("password").value = password
 }
-
-var arr = [
-   [1,2,3],
-   [4,5,6],
-   [7,8,9],
-   [[10,11,12], 13, 14]
- ];
- arr[3]; // equals [[10,11,12], 13, 14]
- arr[3][0]; // equals [10,11,12]
- arr[3][0][1]; // equals 11
-
- console.log(arr[3][0])
